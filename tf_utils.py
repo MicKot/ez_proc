911d8bcbd79180a9a6943c2cf744df1c8227b954
MicKot <kotarba.michael@gmail.com>
2019-02-18 23:06:02 +0100

    added pandas_utils, tf_utils, stat_utils

---
+++ tf_utils.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+def load_graph(path):   
+    with tf.gfile.GFile(path, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(
+            graph_def,
+            input_map=None,
+            return_elements=None,
+            name=""
+            )
+        return graph
