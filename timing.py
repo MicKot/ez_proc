461e8d4391ef405192cbe06d325545e12f9fcea0
MicKot <kotarba.michael@gmail.com>
2019-02-23 15:58:27 +0100

    new modelu timing.py

---
+++ timing.py
@@ -0,0 +1,42 @@
+import time
+
+
+def timer_decorator(func):
+    def time_(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print('Time elapsed:{:.5f}s'.format(end-start))
+        return result
+    return time_
+
+
+class Timer():
+    def __init__(self):
+        self.events = {}
+    
+    def register(self, event_name=None):
+        self.timer_start = time.time()
+        if event_name is not None:
+            self.events[event_name] = self.timer_start
+    
+    def get_now(self, event_name=None):
+        if not hasattr(self, 'timer_start'):
+            print('Error: timer not started, next time start it, bucko')
+            return
+        self.timer_end = time.time()
+        if event_name is not None:
+            print('Time elapsed since event {}: {:.5f}s'.format(event_name, self.timer_end - self.events[event_name]))
+        else:
+            print('Time elapsed since last event: {:.5f}s'.format(self.timer_end - self.timer_start))
+    
+    def get_2(self, first_event_name, second_event_name):
+        started = self.events[first_event_name]
+        ended = self.events[second_event_name]
+        print('Time elapsed between {} and {} {:.5f}s'.format(first_event_name, second_event_name, ended - started))
+
+    def reset_timer(self):
+        self.events = {}
+
+        
+    
