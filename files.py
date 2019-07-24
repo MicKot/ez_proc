d4f405fbddddeec31d5f28345865999809bfbcc8
Michał Kotarba <michal.kotarba@codahead.com>
2019-07-24 15:17:27 +0200

    improved buckets reading

--- files.py
+++ files.py
@@ -7,7 +7,12 @@ def read_images_from_directory_into_buckets(dir, extensions=("jpg", "png", "jpeg
     buckets = []
 
     for root, dirs, files in os.walk(dir):
+        if root == dir:
+            continue
+        paths = []
         for file in files:
             if file.endswith(extensions):
                 full_path = os.path.join(root, file)
+                paths.append(full_path)
+        buckets.append(paths)
     return buckets
